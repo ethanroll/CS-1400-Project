bcb088f663003353cf7215537b3042c9e4abffe5
nickmscott24 <nickmscott24@gmail.com>
2024-11-13 13:52:53 -0800

    Update RestaurantCheckManager.java
    updated the check for quitting and added the actual tip distribution

--- RestaurantCheckManager.java
+++ RestaurantCheckManager.java
@@ -12,14 +12,24 @@ public class RestaurantCheckManager {
 		}
 		catch (NumberFormatException e) { //if the input cannot be converted to a double
 			System.out.println("Input must be a number");
-			return getAmount(scnr);
+			return getAmount(scnr); //allows user to try again
 		}
+	}
+	
+	public static boolean ifQuitting(Scanner scnr) {
+		String quitPrompt = scnr.next();
+		scnr.nextLine(); //clears newline character
+		if (quitPrompt.equalsIgnoreCase("y"))
+			return true;
+		if (quitPrompt.equalsIgnoreCase("n"))
+			return false;
+		System.out.println("Input must be \"y\" or \"n\"");
+		return ifQuitting(scnr); //allows user to try again
 	}
 	
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
 		boolean quit = false;
-		String quitPrompt;
 		double saleAmount;
 		double tipAmount;
 		double totalAmount;
@@ -50,16 +60,36 @@ public class RestaurantCheckManager {
 			System.out.println("Total pooled tip so far: " + totalTipAmount);
 			
 			System.out.println("Do you want to stop (y,n):");
-			quitPrompt = scnr.next();
-			if (quitPrompt.equals("y"))
-				quit = true;
+			quit = ifQuitting(scnr);	
 		}
 		scnr.close();
-		System.out.println("The total sale amount is: " + totalSaleAmount);
-		System.out.println("The total pooled tip amount is: " + totalTipAmount);
+		System.out.printf("The total sale amount is: $%.2f\n", totalSaleAmount);
+		System.out.printf("The total pooled tip amount is: $%.2f\n", totalTipAmount);
+		System.out.println();
+		printTipSplits(totalTipAmount);
 	}
 	
 	public static void printTipSplits(double totalTipAmount) {
-		//FIXME: add something
+		final double PERCENT_SERVER = 0.70;
+		final int NUM_SERVERS = 3;
+		final double PERCENT_CHEF = 0.04;
+		final double PERCENT_SOUS_CHEF = 0.03;
+		final double PERCENT_KITCHEN_AID = 0.03;
+		//for the sake of simplicity, host/hostess has been simplified to just host
+		final double PERCENT_HOST = 0.10; 
+		final double PERCENT_BUSSER = 0.10;
+		
+		double tipPerServer = totalTipAmount * PERCENT_SERVER / NUM_SERVERS;
+		System.out.printf("$%.2f goes to each server\n", tipPerServer);
+		double tipChef = totalTipAmount * PERCENT_CHEF;
+		System.out.printf("$%.2f goes to the chef\n", tipChef);
+		double tipSousChef = totalTipAmount * PERCENT_SOUS_CHEF;
+		System.out.printf("$%.2f goes to the sous-chef\n", tipSousChef);
+		double tipKitchenAid = totalTipAmount * PERCENT_KITCHEN_AID;
+		System.out.printf("$%.2f goes to the kitchen aid\n", tipKitchenAid);
+		double tipHost = totalTipAmount * PERCENT_HOST;
+		System.out.printf("$%.2f goes to the host/hostess\n", tipHost);
+		double tipBusser = totalTipAmount * PERCENT_BUSSER;
+		System.out.printf("$%.2f goes to the busser\n", tipBusser);
 	}
 }
