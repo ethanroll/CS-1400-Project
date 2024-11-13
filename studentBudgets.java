fcb45047abfd43ebda1e464777c4d90e37902219
nickmscott24 <nickmscott24@gmail.com>
2024-11-12 20:16:43 -0800

    Update studentBudgets.java

--- studentBudgets.java
+++ studentBudgets.java
@@ -1,3 +1,33 @@
-public static void main(String[] args) {
-  Hello
+import java.util.Scanner;
+
+public class RestaurantCheckManager {
+	/**
+	 * Start by creating a class called RestaurantCheckManager
+	 * Write a loop that keeps running until the manager the manager asks to terminate
+	 * Inside the loop, prompt the manager to enter the sale amount
+	 * Then prompt the manager to enter the tip amount
+	 * Finally, prompt the manager to enter the total amount
+	 * Calculate the total sale amount, the total tip amount, and the number of checks
+	 * Pay attention to the edge cases listed above
+	 * Ask the manager if they want to stop
+	 * If they type ‘y’ or ‘Y’, terminate the loop
+	 * Else continue
+	 * After that the program should display the total sale amount and the total tip amount
+	 */
+	
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+		boolean quit = false;
+		double saleAmount;
+		double tipAmount;
+		double totalAmount;
+		double totalSaleAmount;
+		double totalTipAmount;
+		int numberOfChecks;
+		
+		while (true) {
+			System.out.println("Please enter the sale amount");
+			
+		}
+	}
 }
